05b1001f413c3ffc29fec26a0722786dd017550f
Lucas Brendel <lucasbrendel@gmail.com>
2024-12-20 21:48:55 -0600

    fix: adjusting commands

--- sparky/config.py
+++ sparky/config.py
@@ -1,23 +1,25 @@
 
+from typing import Annotated
 import typer
+from typer import Argument
 
 app = typer.Typer()
 
 
-@app.command()
-def get():
+@app.command(help="Get configuration value of specified key")
+def get(key: Annotated[str, Argument()]):
     pass
 
-@app.command()
-def set():
+@app.command(help="Set a value to a key")
+def set(key: Annotated[str, Argument()], value):
     pass
 
 
-@app.command()
+@app.command(help="List out all configuration key value pairs")
 def list():
     pass
 
 
-@app.command()
-def remove():
+@app.command(help="Remove a configuration key/value pair from the configuration file.")
+def remove(key: Annotated[str, Argument()]):
     pass
--- sparky/sparky.py
+++ sparky/sparky.py
@@ -1,5 +1,6 @@
-from typing import List, Optional
-
+from typing import Annotated, List, Optional
+import os
+import sys
 import config
 import packet
 import task
@@ -8,46 +9,64 @@ from rich import print
 import rich.traceback
 from typer_shell import make_typer_shell
 
-import test
-
 # Make Rich be the stack trace handler
 rich.traceback.install(show_locals=True)
 
-shell = make_typer_shell(prompt=">>")
+shell = make_typer_shell(intro="Welcome to Sparky!")
 
 app = typer.Typer()
+
 app.add_typer(task.app, name="task")
 app.add_typer(packet.app, name="packet")
 app.add_typer(shell, name="shell")
-app.add_typer(test.app, name="test")
 app.add_typer(config.app, name="config")
 
+shell.add_typer(task.app, name="task")
+shell.add_typer(packet.app, name="packet")
+shell.add_typer(config.app, name="config")
 
-@app.command()
-@shell.command()
+
+@app.command(help="Print information about Sparky")
+@shell.command(help="Print information about Sparky")
 def about():
     print("Sparky")
 
 
 @app.command(help="Create a new Sparky project environment")
-def init(path: str):
+def init(path: Annotated[str, typer.Argument()]):
     print("[green]Creating new project[/green]")
 
 
-@app.command(help="Restart the environment")
-def reload():
+@app.command(help="Upgrade Sparky environment to latest version.")
+def upgrade():
+    pass
+
+@app.command(help="List registered modules")
+@shell.command(help="List registered modules")
+def modules():
     pass
 
 
-@app.command()
+@shell.command(help="Restart the environment")
+def reload():
+    os.execv(sys.executable, sys.argv)
+
+
+@shell.command(help="Clear the shell screen")
 def clear():
     pass
 
 
-@app.command()
-def upgrade():
+@app.command(help="Execute a series or suite of tests.")
+@shell.command(help="Execute a series or suite of tests.")
+def test(tests: List[str], suites: Annotated[str, typer.Option(help="Test suites to run")],
+         loop: Optional[int] = None, dryrun: Optional[bool] = False,
+         skip_preflight: Optional[bool] = False, skip_cleanup: Optional[bool] = False):
     pass
 
+@app.callback()
+def main(env: str = "."):
+    print(f"Loading environment {os.path.realpath(env)}")
 
 if __name__ == "__main__":
     app()
